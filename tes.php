023269efc652578e64c0c5c9295aafe9c6d62946
Husain <ochy22@gmail.com>
2021-10-04 13:18:54 +0800

    no message

---
+++ tes.php
@@ -0,0 +1,17 @@
+<?php
+
+$panjangSisi1= 10;
+$panjangSisi2= 6;
+$tinggi = 5;
+
+$jari2=10;
+define("PHI", 3.14);
+$luasPermukaanBola = 4 * PHI * ($jari2**2);
+echo "Luas Permukaan Bola = " .$luasPermukaanBola;
+
+
+
+// $luas = (0.5 * ( ($panjangSisi1 + $panjangSisi2)* $tinggi));
+// echo "Luas Trapesium = ". $luas;
+
+?>
